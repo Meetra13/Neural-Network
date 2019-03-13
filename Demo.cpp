714956ba4a62616c6f9ffdaa65dd865e903daf2c
Meetra13 <meet@student.chalmers.se>
2019-03-13 23:48:08 +0100

    First commit to the repo

---
+++ Demo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+void func() {
+  static int a = 0;
+  cout << ++a << endl;
+}
+
+int main(void){
+
+  for (int i = 0; i < 20; i++) {
+   func();
+  }
+
+  {
+    int a = 1234;
+    cout << a << endl;
+  }
+
+  {
+    int a = 41234;
+    cout << a << endl;
+  }
+
+  return 0;
+}
